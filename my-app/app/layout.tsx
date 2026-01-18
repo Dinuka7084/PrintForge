7794d0c7a43dd4378cc7bbe17541f5be678b4b18
Dinuka Lakmal <dinukalakmalwich7084@gmail.com>
2026-01-18 18:40:08 +0530

    navBar created

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import NavBar from "./components/NavBar";
 
 export default function RootLayout({
   children,
@@ -9,6 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <NavBar />
         {children}
       </body>
     </html>
